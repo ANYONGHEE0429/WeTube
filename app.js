5f1e8b97b24f7f5dc819fcc12ccd6f703c54321b
Yonghee <53254016+YongheeAn@users.noreply.github.com>
2020-07-24 23:11:08 +0900

    Router

--- app.js
+++ app.js
@@ -3,18 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers./globalRouter";
+import routes from "./routes";
 
 var app = express();
-
-const PORT = 4000;
-
-function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
-
-const handleHome = (req, res) => res.send("hello from home");
-
-const handleProfile = (req, res) => res.send("This is my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -22,13 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-//if use res.send can kill the connection
-const middleware = (req, res, next) => {
-  res.send("not hanppening");
-};
+//sombody calls user,I want to use whole Router
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.get("/", middleware, handleHome);
-
-app.get("/profile", handleProfile);
-
-app.listen(4000, handleListening);
+export default app;
