4d3f3ac2513f324c914884e3fd0c815445b3d564
Yonghee <53254016+YongheeAn@users.noreply.github.com>
2020-07-31 17:18:44 +0900

    Revert "show videos"
    This reverts commit 4552b49ae486bcee5fd78038c58ae91a3bc0f2de.

--- db.js
+++ db.js
@@ -4,8 +4,7 @@ export const videos = [
     title: "Video awesome",
     description: "This is something I love",
     views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
     creator: {
       id: 121212,
       name: "Nicolas",
@@ -17,8 +16,7 @@ export const videos = [
     title: "Video super",
     description: "This is something I love",
     views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
     creator: {
       id: 121212,
       name: "Nicolas",
@@ -30,8 +28,7 @@ export const videos = [
     title: "Video nice",
     description: "This is something I love",
     views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
     creator: {
       id: 121212,
       name: "Nicolas",
@@ -43,8 +40,7 @@ export const videos = [
     title: "Video perfect",
     description: "This is something I love",
     views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
     creator: {
       id: 121212,
       name: "Nicolas",
