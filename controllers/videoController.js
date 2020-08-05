297d16eb35b5599e0b15db16c18a53ec4ca8aa28
Yonghee <53254016+YongheeAn@users.noreply.github.com>
2020-08-05 22:15:08 +0900

    Update videoController.js

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
 import { videos } from "../db";
+import routes from "../routes";
+
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
 };
@@ -10,8 +12,16 @@ export const search = (req, res) => {
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+//Upload & Save Video
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description },
+  } = req;
+  res.redirect(routes.videoDetail());
+};
 
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
