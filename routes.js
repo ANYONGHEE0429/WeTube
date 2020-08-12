533a597445d8a3d17233c58f04b3fdaf58c9c379
Yonghee <53254016+YongheeAn@users.noreply.github.com>
2020-08-12 15:41:33 +0900

    Error that should be careful

--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ const CHANGE_PASSWORD = "/change-password";
 //videos
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
-const VIDEO_DETAIL = "/:id ";
+const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
