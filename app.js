177fdd37ee64cf4a48a17692bd6d7bf73c023224
Yonghee <53254016+YongheeAn@users.noreply.github.com>
2020-08-26 22:27:54 +0900

    Resetting

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = express();
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads")); //look for a file not any kind of controller, view
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
